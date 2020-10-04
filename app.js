22d3e29bf8a57118d0c2c204699f2a826ae6d101
Vitor <vitor1184@hotmail.com>
2020-10-04 20:34:59 -0300

    Adicionado tratamento de erro com Morgan

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 
 const rotaProdutos = require("./routes/produtos");
 const rotaPedidos = require("./routes/pedidos");
 
-app.use("/produtos", rotaProdutos);
+app.use(morgan('dev'));
 
+app.use("/produtos", rotaProdutos);
 app.use("/pedidos", rotaPedidos);
+
+//Tratamento de erro para rotas
+app.use((req, res, next) => {
+    const erro = new Error("Não encontrado");
+    erro.status = 404;
+    next(erro);
+});
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    return res.send({
+        erro: {
+            mensagem: err.message
+        }
+    });
+});
 
 module.exports = app;
